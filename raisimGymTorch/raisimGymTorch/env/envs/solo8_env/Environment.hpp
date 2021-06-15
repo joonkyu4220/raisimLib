67a54935b249f93eb3158999be5b160c42caecb2
Zhaoming Xie <zxieaa@gmail.com>
2021-06-15 10:22:45 -0700

    add solo8

---
+++ raisimGymTorch/raisimGymTorch/env/envs/solo8_env/Environment.hpp
@@ -0,0 +1,144 @@
+#pragma once
+
+#include <stdlib.h>
+#include <set>
+#include "../../RaisimGymEnv.hpp"
+
+namespace raisim {
+
+class ENVIRONMENT : public RaisimGymEnv {
+
+ public:
+
+  explicit ENVIRONMENT(const std::string& resourceDir, const Yaml::Node& cfg, bool visualizable) :
+      RaisimGymEnv(resourceDir, cfg), visualizable_(visualizable) {
+
+    /// create world
+    world_ = std::make_unique<raisim::World>();
+
+    /// add objects
+    solo8_ = world_->addArticulatedSystem(resourceDir_+"/solo8_URDF_v4/solo8_v5.urdf");
+    solo8_->setName("solo8");
+    solo8_->setControlMode(raisim::ControlMode::PD_PLUS_FEEDFORWARD_TORQUE);
+    world_->addGround();
+
+    /// get robot data
+    gcDim_ = solo8_->getGeneralizedCoordinateDim();
+    gvDim_ = solo8_->getDOF();
+    nJoints_ = gvDim_ - 6;
+
+    /// initialize containers
+    gc_.setZero(gcDim_); gc_init_.setZero(gcDim_);
+    gv_.setZero(gvDim_); gv_init_.setZero(gvDim_);
+    pTarget_.setZero(gcDim_); vTarget_.setZero(gvDim_); pTarget12_.setZero(nJoints_);
+
+    /// this is nominal configuration of anymal
+    gc_init_ << 0, 0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.0, -0.0, 0.0, -0.0, 0.0, -0.0;
+
+    /// set pd gains
+    Eigen::VectorXd jointPgain(gvDim_), jointDgain(gvDim_);
+    jointPgain.setZero(); jointPgain.tail(nJoints_).setConstant(50.0);
+    jointDgain.setZero(); jointDgain.tail(nJoints_).setConstant(0.2);
+    solo8_->setPdGains(jointPgain, jointDgain);
+    solo8_->setGeneralizedForce(Eigen::VectorXd::Zero(gvDim_));
+
+    /// MUST BE DONE FOR ALL ENVIRONMENTS
+    obDim_ = 26;
+    actionDim_ = nJoints_; actionMean_.setZero(actionDim_); actionStd_.setZero(actionDim_);
+    obDouble_.setZero(obDim_);
+
+    /// action scaling
+    actionMean_ = gc_init_.tail(nJoints_);
+    actionStd_.setConstant(0.5);
+
+    /// Reward coefficients
+    rewards_.initializeFromConfigurationFile (cfg["reward"]);
+
+    /// indices of links that should not make contact with ground
+    footIndices_.insert(solo8_->getBodyIdx("rear_left_lower_leg"));
+    footIndices_.insert(solo8_->getBodyIdx("rear_right_lower_leg"));
+    footIndices_.insert(solo8_->getBodyIdx("front_right_lower_leg"));
+    footIndices_.insert(solo8_->getBodyIdx("front_left_lower_leg"));
+
+    /// visualize if it is the first environment
+    if (visualizable_) {
+      server_ = std::make_unique<raisim::RaisimServer>(world_.get());
+      server_->launchServer();
+      server_->focusOn(solo8_);
+    }
+  }
+
+  void init() final { }
+
+  void reset() final {
+    solo8_->setState(gc_init_, gv_init_);
+    updateObservation();
+  }
+
+  float step(const Eigen::Ref<EigenVec>& action) final {
+    /// action scaling
+    pTarget12_ = action.cast<double>();
+    pTarget12_ = pTarget12_.cwiseProduct(actionStd_);
+    pTarget12_ += actionMean_;
+    pTarget_.tail(nJoints_) = pTarget12_;
+
+    solo8_->setPdTarget(pTarget_, vTarget_);
+
+    for(int i=0; i< int(control_dt_ / simulation_dt_ + 1e-10); i++){
+      if(server_) server_->lockVisualizationServerMutex();
+      world_->integrate();
+      if(server_) server_->unlockVisualizationServerMutex();
+    }
+
+    updateObservation();
+
+    rewards_.record("torque", solo8_->getGeneralizedForce().squaredNorm());
+    rewards_.record("forwardVel", std::min(4.0, bodyLinearVel_[0]));
+
+    return rewards_.sum();
+  }
+
+  void updateObservation() {
+    solo8_->getState(gc_, gv_);
+    raisim::Vec<4> quat;
+    raisim::Mat<3,3> rot;
+    quat[0] = gc_[3]; quat[1] = gc_[4]; quat[2] = gc_[5]; quat[3] = gc_[6];
+    raisim::quatToRotMat(quat, rot);
+    bodyLinearVel_ = rot.e().transpose() * gv_.segment(0, 3);
+    bodyAngularVel_ = rot.e().transpose() * gv_.segment(3, 3);
+
+    obDouble_ << gc_[2], /// body height
+        rot.e().row(2).transpose(), /// body orientation
+        gc_.tail(8), /// joint angles
+        bodyLinearVel_, bodyAngularVel_, /// body linear&angular velocity
+        gv_.tail(8); /// joint velocity
+  }
+
+  void observe(Eigen::Ref<EigenVec> ob) final {
+    /// convert it to float
+    ob = obDouble_.cast<float>();
+  }
+
+  bool isTerminalState(float& terminalReward) final {
+    terminalReward = float(terminalRewardCoeff_);
+
+    /// if the contact body is not feet
+    for(auto& contact: solo8_->getContacts())
+      if(footIndices_.find(contact.getlocalBodyIndex()) == footIndices_.end())
+        return true;
+
+    terminalReward = 0.f;
+    return false;
+  }
+
+ private:
+  int gcDim_, gvDim_, nJoints_;
+  bool visualizable_ = false;
+  raisim::ArticulatedSystem* solo8_;
+  Eigen::VectorXd gc_init_, gv_init_, gc_, gv_, pTarget_, pTarget12_, vTarget_;
+  double terminalRewardCoeff_ = -10.;
+  Eigen::VectorXd actionMean_, actionStd_, obDouble_;
+  Eigen::Vector3d bodyLinearVel_, bodyAngularVel_;
+  std::set<size_t> footIndices_;
+};
+}
--- raisimPy/examples/robots.py
+++ raisimPy/examples/robots.py
@@ -1,5 +1,7 @@
 import os
 import numpy as np
+import sys
+sys.path.append('/home/zhaoming/Documents/open_robot/raisim_build/lib')
 import raisimpy as raisim
 import time
 
@@ -8,6 +10,7 @@ anymal_urdf_file = os.path.dirname(os.path.abspath(__file__)) + "/../../rsc/anym
 laikago_urdf_file = os.path.dirname(os.path.abspath(__file__)) + "/../../rsc/laikago/laikago.urdf"
 atlas_urdf_file = os.path.dirname(os.path.abspath(__file__)) + "/../../rsc/atlas/robot.urdf"
 monkey_file = os.path.dirname(os.path.abspath(__file__)) + "/../../rsc/monkey/monkey.obj"
+solo8_urdf_file = os.path.dirname(os.path.abspath(__file__)) + "/../../rsc/solo8_URDF_v4/solo8_v5.urdf"
 dummy_inertia = np.zeros([3, 3])
 np.fill_diagonal(dummy_inertia, 0.1)
 
@@ -16,61 +19,72 @@ world.setTimeStep(0.001)
 server = raisim.RaisimServer(world)
 ground = world.addGround()
 
-anymal = world.addArticulatedSystem(anymal_urdf_file)
-anymal.setName("anymal")
-anymal_nominal_joint_config = np.array([0, -1.5, 0.54, 1.0, 0.0, 0.0, 0.0, 0.03, 0.4, -0.8,
-                                        -0.03, 0.4, -0.8, 0.03, -0.4, 0.8, -0.03, -0.4, 0.8])
-anymal.setGeneralizedCoordinate(anymal_nominal_joint_config)
-anymal.setPdGains(200*np.ones([18]), np.ones([18]))
-anymal.setPdTarget(anymal_nominal_joint_config, np.zeros([18]))
+# anymal = world.addArticulatedSystem(anymal_urdf_file)
+# anymal.setName("anymal")
+# anymal_nominal_joint_config = np.array([0, -1.5, 0.54, 1.0, 0.0, 0.0, 0.0, 0.03, 0.4, -0.8,
+#                                         -0.03, 0.4, -0.8, 0.03, -0.4, 0.8, -0.03, -0.4, 0.8])
+# anymal.setGeneralizedCoordinate(anymal_nominal_joint_config)
+# anymal.setPdGains(200*np.ones([18]), np.ones([18]))
+# anymal.setPdTarget(anymal_nominal_joint_config, np.zeros([18]))
 
-laikago = world.addArticulatedSystem(laikago_urdf_file)
-laikago.setName("laikago")
-laikago_nominal_joint_config = np.array([0, 1.5, 0.48, 1, 0.0, 0.0, 0.0, 0.0, 0.5, -1, 0, 0.5, -1,
-                                         0.00, 0.5, -1, 0, 0.5, -0.7])
-laikago.setGeneralizedCoordinate(laikago_nominal_joint_config)
-laikago.setPdGains(200*np.ones([18]), np.ones([18]))
-laikago.setPdTarget(laikago_nominal_joint_config, np.zeros([18]))
+# laikago = world.addArticulatedSystem(laikago_urdf_file)
+# laikago.setName("laikago")
+# laikago_nominal_joint_config = np.array([0, 1.5, 0.48, 1, 0.0, 0.0, 0.0, 0.0, 0.5, -1, 0, 0.5, -1,
+#                                          0.00, 0.5, -1, 0, 0.5, -0.7])
+# laikago.setGeneralizedCoordinate(laikago_nominal_joint_config)
+# laikago.setPdGains(200*np.ones([18]), np.ones([18]))
+# laikago.setPdTarget(laikago_nominal_joint_config, np.zeros([18]))
 
-atlas = world.addArticulatedSystem(atlas_urdf_file)
-atlas.setName("atlas")
-atlas_nominal_joint_config = np.zeros(atlas.getGeneralizedCoordinateDim())
-atlas_nominal_joint_config[2] = 1.5
-atlas_nominal_joint_config[3] = 1
-atlas.setGeneralizedCoordinate(atlas_nominal_joint_config)
+# atlas = world.addArticulatedSystem(atlas_urdf_file)
+# atlas.setName("atlas")
+# atlas_nominal_joint_config = np.zeros(atlas.getGeneralizedCoordinateDim())
+# atlas_nominal_joint_config[2] = 1.5
+# atlas_nominal_joint_config[3] = 1
+# atlas.setGeneralizedCoordinate(atlas_nominal_joint_config)
+
+solo8 = world.addArticulatedSystem(solo8_urdf_file)
+solo8.setName("solo8")
+solo8_nominal_joint_config = np.zeros(solo8.getGeneralizedCoordinateDim())
+print(solo8_nominal_joint_config)
+solo8_nominal_joint_config[0] = 0
+solo8_nominal_joint_config[2] = 5
+solo8_nominal_joint_config[3] = 1
+solo8.setGeneralizedCoordinate(solo8_nominal_joint_config)
+solo8.setPdGains(200*np.ones([14]), np.ones([14]))
+solo8.setPdTarget(solo8_nominal_joint_config, np.zeros([14]))
 
 server.launchServer(8080)
 
-for i in range(5):
-    for j in range(5):
-        object_type = (i + j*6) % 5
+# for i in range(5):
+#     for j in range(5):
+#         object_type = (i + j*6) % 5
 
-        if object_type == 0:
-            obj = world.addMesh(monkey_file, 5.0, dummy_inertia, np.array([0, 0, 0]), 0.3)
-        elif object_type == 1:
-            obj = world.addCylinder(0.2, 0.3, 2.0)
-        elif object_type == 2:
-            obj = world.addCapsule(0.2, 0.3, 2.0)
-        elif object_type == 3:
-            obj = world.addBox(0.4, 0.4, 0.4, 2.0)
-        else:
-            obj = world.addSphere(0.3, 2.0)
+#         if object_type == 0:
+#             obj = world.addMesh(monkey_file, 5.0, dummy_inertia, np.array([0, 0, 0]), 0.3)
+#         elif object_type == 1:
+#             obj = world.addCylinder(0.2, 0.3, 2.0)
+#         elif object_type == 2:
+#             obj = world.addCapsule(0.2, 0.3, 2.0)
+#         elif object_type == 3:
+#             obj = world.addBox(0.4, 0.4, 0.4, 2.0)
+#         else:
+#             obj = world.addSphere(0.3, 2.0)
 
-        obj.setPosition(i-2.5, j-2.5, 5)
+#         obj.setPosition(i-2.5, j-2.5, 5)
 
 time.sleep(2)
 world.integrate1()
 
 ### get dynamic properties
 # mass matrix
-mass_matrix = anymal.getMassMatrix()
+# mass_matrix = anymal.getMassMatrix()
 # non-linear term (gravity+coriolis)
-non_linearities = anymal.getNonlinearities()
+# non_linearities = anymal.getNonlinearities()
 # Jacobians
-jaco_foot_lh_linear = anymal.getDenseFrameJacobian("LF_ADAPTER_TO_FOOT")
-jaco_foot_lh_angular = anymal.getDenseFrameRotationalJacobian("LF_ADAPTER_TO_FOOT")
+# jaco_foot_lh_linear = anymal.getDenseFrameJacobian("LF_ADAPTER_TO_FOOT")
+# jaco_foot_lh_angular = anymal.getDenseFrameRotationalJacobian("LF_ADAPTER_TO_FOOT")
 
-for i in range(5000):
+for i in range(5000000):
     world.integrate()
 
 server.killServer()
