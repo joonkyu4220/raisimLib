02e22be322fa151db5809a4e07638e2f9902efb3
Zhaoming Xie <zxieaa@gmail.com>
2021-11-15 10:40:20 -0800

    3d humanoid juggling example

--- raisimPy/examples/robots.py
+++ raisimPy/examples/robots.py
@@ -11,6 +11,8 @@ laikago_urdf_file = os.path.dirname(os.path.abspath(__file__)) + "/../../rsc/lai
 atlas_urdf_file = os.path.dirname(os.path.abspath(__file__)) + "/../../rsc/atlas/robot.urdf"
 monkey_file = os.path.dirname(os.path.abspath(__file__)) + "/../../rsc/monkey/monkey.obj"
 solo8_urdf_file = os.path.dirname(os.path.abspath(__file__)) + "/../../rsc/solo8_URDF_v6/solo8.urdf"
+virtual_human_urdf_file = os.path.dirname(os.path.abspath(__file__)) + "/../../rsc/virtual_human.urdf"
+walker_urdf_file = os.path.dirname(os.path.abspath(__file__)) + "/../../rsc/humanoid.urdf"
 dummy_inertia = np.zeros([3, 3])
 np.fill_diagonal(dummy_inertia, 0.1)
 
@@ -42,20 +44,52 @@ ground = world.addGround()
 # atlas_nominal_joint_config[3] = 1
 # atlas.setGeneralizedCoordinate(atlas_nominal_joint_config)
 
-solo8 = world.addArticulatedSystem(solo8_urdf_file)
-solo8.setName("solo8")
-solo8_nominal_joint_config = np.zeros(solo8.getGeneralizedCoordinateDim())
-print(solo8_nominal_joint_config)
-solo8_nominal_joint_config[0] = 0
-solo8_nominal_joint_config[2] = 0.35
-solo8_nominal_joint_config[3] = 1
-solo8.setGeneralizedCoordinate(solo8_nominal_joint_config)
-solo8.setPdGains(2*np.ones([14]), 0.2*np.ones([14]))
-solo8.setPdTarget(solo8_nominal_joint_config, np.zeros([14]))
+# solo8 = world.addArticulatedSystem(solo8_urdf_file)
+# solo8.setName("solo8")
+# solo8_nominal_joint_config = np.zeros(solo8.getGeneralizedCoordinateDim())
+# print(solo8_nominal_joint_config)
+# solo8_nominal_joint_config[0] = 5
+# solo8_nominal_joint_config[2] = 0.35
+# solo8_nominal_joint_config[3] = 1
+# solo8.setGeneralizedCoordinate(solo8_nominal_joint_config)
+# solo8.setPdGains(2*np.ones([14]), 0.2*np.ones([14]))
+# solo8.setPdTarget(solo8_nominal_joint_config, np.zeros([14]))
+
+# virtual_human = world.addArticulatedSystem(virtual_human_urdf_file)
+# virtual_human.setName("virtual_human")
+# virtual_human_nominal_joint_config = np.array([0., 0., 0., 1., 0., 0., 0., 1., 0., 0., 0., 1., 0., 0., 0., 1., 0., 0., 0., 1., 0., 0., 0., 1.,
+#  0., 0., 0., 1., 0., 0., 0., 1., 0., 0., 0., 1., 0., 0., 0., 1., 0., 0., 0., 1., 0., 0., 0., 1.,
+#  0., 0., 0., 1., 0., 0., 0., 1., 0., 0., 0., 1., 0., 0., 0., 1., 0., 0., 0., 1., 0., 0., 0., 1.,
+#  0., 0., 0.,])
+# print(virtual_human.getGeneralizedCoordinateDim())
+# print(virtual_human.getDOF())
+# virtual_human_nominal_joint_config[0] = 0
+# virtual_human_nominal_joint_config[2] = 2
+# virtual_human.setGeneralizedCoordinate(virtual_human_nominal_joint_config)
+# virtual_human.setPdGains(2*np.ones([virtual_human.getDOF()]), 0.2*np.ones([virtual_human.getDOF()]))
+# virtual_human.setPdTarget(virtual_human_nominal_joint_config, np.zeros([virtual_human.getDOF()]))
+
+walker = world.addArticulatedSystem(walker_urdf_file)
+walker.setName("walker")
+print(walker.getGeneralizedCoordinate())
+walker_nominal_joint_config = np.zeros(walker.getGeneralizedCoordinateDim())
+walker_nominal_joint_config[:] = [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0,
+ 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0]
+print(walker.getGeneralizedCoordinateDim())
+walker_nominal_joint_config[2] = 1.707
+walker_nominal_joint_config[3] = 0.707
+walker_nominal_joint_config[4] = 0.707
+
+walker.setGeneralizedCoordinate(walker_nominal_joint_config)
+walker.setPdGains(0*np.ones([walker.getDOF()]), 0*np.ones([walker.getDOF()]))
+
+world.setMaterialPairProp("default", "ball", 1.0, 0.8, 0.01)
+obj = world.addSphere(0.11, 0.45, material="ball")
+obj.setPosition(0., 0, 4)
 
 server.launchServer(8080)
-obj = world.addSphere(0.2, 0.8)
-obj.setPosition(0, 0, 0.2)
+# obj = world.addSphere(0.2, 0.8)
+# obj.setPosition(0, 0, 0.2)
 # import ipdb; ipdb.set_trace()
 
 # for i in range(5):
@@ -87,55 +121,67 @@ world.integrate1()
 # jaco_foot_lh_linear = anymal.getDenseFrameJacobian("LF_ADAPTER_TO_FOOT")
 # jaco_foot_lh_angular = anymal.getDenseFrameRotationalJacobian("LF_ADAPTER_TO_FOOT")
 
-reference = np.zeros(15)
-reference[2] = 0.55
-reference[3] = 0.7071068
-reference[5] = -0.7071068
+reference = np.zeros(walker.getGeneralizedCoordinateDim())
+# reference[2] = 2
+# reference[3] = 1
+# reference[5] = 0
 t = 0
 period = 30
 
+import pandas as pd
+from axis_to_quaternion import *
+
+with open('Input.txt') as f:
+	data = f.readlines()
+
+def coordinate_transform(q):
+	q[0:4] = q[[0, 3, 1, 2]]
+	q[1] = -q[1]
+	q[3] = -q[3]
+	return q
+
+def rotate_translation(x):
+	x[0:3] = x[[2, 0, 1]]
+	# x[0] = -x[0]
+	x[1] = -x[1]
+	return x
+
+
 for i in range(5000000):
-	for j in range(4):
-		if t <= period / 2:
-			if j == 0 or j == 3:
-				reference[7+j*2] = 0.65
-				reference[8+j*2] = -1.0
-			else:
-				reference[7+j*2] = 0.65
-				reference[8+j*2] = -1.0 - 0.7 * np.sin(2.0*np.pi*t/period)
-		else:
-			if j == 0 or j == 3:
-				reference[7+j*2] = 0.65
-				reference[8+j*2] = -1.0 - 0.7 * np.sin(2.0*np.pi*t/period - np.pi)
-			else:
-				reference[7+j*2] = 0.65
-				reference[8+j*2] = -1.0
-	reference[7+4] = 1.57
-	reference[7+6] = 1.57
-	reference[8+4] = 0
-	reference[8+6] = 0
-	reference[2] = 0.53 + 0.4
-	if t <= period / 2:
-		reference[7+4] = 1.57 - 0.7 * np.sin(2.0*np.pi*t/period)
-		reference[8+4] = 0 + 0.7 * np.sin(2.0*np.pi*t/period)
-	else:
-		reference[7+6] = 1.57 - 0.7 * np.sin(2.0*np.pi*t/period - np.pi)
-		reference[8+6] = 0 + 0.7 * np.sin(2.0*np.pi*t/period - np.pi)
-	reference[7] = 1.57 + np.sin(2.0*np.pi*t/period)
-	reference[9] = 1.57 + np.sin(2.0*np.pi*t/period + np.pi)
-	if i == 0:
-		solo8.setGeneralizedCoordinate(reference)
-	solo8.setPdTarget(reference, np.zeros([14]))
 
-	fl_id = solo8.getFrameIdxByName("FL_ANKLE")
+	frame =  int(i / 2)
 
-	print(solo8.getFrameVelocity(fl_id))
+	from scipy.spatial.transform import Rotation as R
 
-	# import ipdb; ipdb.set_trace()
+	frame_data = np.asarray(data[frame].split(' ')).astype(float)
 
-	# for _ in range(10):
+	# print(frame_data)
+	reference[0:15] = frame_data[0:15]
+	reference[15:19] = frame_data[15:19]
+	reference[19] = frame_data[19]
+	reference[20:24] = frame_data[24:28]
+	reference[24] = frame_data[28]
+	reference[25:43] = frame_data[33:51]
+
+	reference[19] = -reference[19] / 180 * 3.1415
+	reference[24] = -reference[24] / 180 * 3.1415
+	reference[29] = -reference[29] / 180 * 3.1415
+	reference[38] = -reference[38] / 180 * 3.1415
+	reference[3:7] = coordinate_transform(reference[3:7])
+	reference[15:19] = coordinate_transform(reference[15:19])
+	reference[20:24] = coordinate_transform(reference[20:24])
+	reference[25:29] = coordinate_transform(reference[25:29])
+	reference[30:34] = coordinate_transform(reference[30:34])
+	reference[34:38] = coordinate_transform(reference[34:38])
+	reference[39:43] = coordinate_transform(reference[39:43])
+	reference[0:3] = rotate_translation(reference[0:3])
+	# reference[2] += 0
+	walker.setGeneralizedCoordinate(reference)
+	walker.setPdTarget(reference, np.zeros([walker.getDOF()]))
+	obj.setPosition(frame_data[53], -frame_data[51], frame_data[52])
+	# print(reference[0:3], frame_data[51], frame_data[53], frame_data[52])
 	world.integrate()
-	import time; time.sleep(0.02)
+	import time; time.sleep(0.01)
 	t += 1
 	if t > period:
 		t = 0
