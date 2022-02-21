ae935b3757653c5284a058ad2c92399b17646ac4
ZhaomingXie <zxieaa@gmail.com>
2022-02-21 09:16:23 -0800

    refactor visulize mocap

--- raisimPy/examples/visualize_mocap.py
+++ raisimPy/examples/visualize_mocap.py
@@ -7,8 +7,8 @@ sys.path.append('/home/zhaoming/Documents/open_robot/raisim_build/lib')
 import raisimpy as raisim
 
 from fairmotion.data import bvh
-BVH_FILENAME = "../../../../../Downloads/MotionCapture/Mocap/Loco/RunSideBack1.bvh"
-motion = bvh.load(BVH_FILENAME)
+# BVH_FILENAME = "../../../../../Downloads/MotionCapture/Mocap/Loco/RunSideBack1.bvh"
+# motion = bvh.load(BVH_FILENAME)
 
 world = raisim.World()
 world.setTimeStep(0.001)
@@ -32,7 +32,7 @@ def coordinate_transform(q):
 	return q
 
 def process_mocap(mocap_file):
-	motion = bvh.load(BVH_FILENAME)
+	motion = bvh.load(mocap_file)
 	matrix = motion.to_matrix(local=False)[:, :, :, :]
 	local_matrix = motion.to_matrix(local=True)[:, :, : ,:]
 	pos = matrix[:, :, :3, 3]
@@ -73,7 +73,22 @@ def process_mocap(mocap_file):
 		for j in range(23):
 			bone_velocity[i, j, :] = store_data[i+1, j, :] - store_data[i, j, :]
 
-	return root_pos, root_rot, root_linear_vel, root_angular_vel, store_data, bone_velocity
+	#kd tree features
+	features = np.zeros((num_frames - 60, 3 * 4 + 3 * 5))
+	for i in range(num_frames - 60):
+		frames = [19, 39, 59]
+		root_pos_feature = np.zeros(3)
+		for j in range(3):
+			features[i, j*4:j*4+2] = root_rot[i, :, :].T.dot(root_pos[frames[j] + i] - root_pos[i])[[0, 2]]
+			features[i, j*4+2:j*4+4] = root_rot[i+frames[j], :, :].dot(root_rot[i, :, :].T).dot(np.array([1, 0, 0]))[[0, 2]]
+			#features[i, j*9+6:j*9+9] = root_angular_vel[i+j, :, 1].copy()
+		features[i, 3 * 4: 3 * 4 + 3] = bone_velocity[i, 0, :].flatten().copy()
+		features[i, 3 * 4 + 3: 3 * 4 + 6] = store_data[i, 17, :].flatten().copy()
+		features[i, 3 * 4 + 6: 3 * 4 + 9] = store_data[i, 21, :].flatten().copy()
+		features[i, 3 * 4 + 9: 3 * 4 + 12] = bone_velocity[i, 17, :].flatten().copy()
+		features[i, 3 * 4 + 12: 3 * 4 + 15] = bone_velocity[i, 21, :].flatten().copy()
+
+	return root_pos, root_rot, root_linear_vel, root_angular_vel, store_data, bone_velocity, local_matrix, features
 
 
 def set_human_pose(index, hip_pose=None, hip_rot=None):
@@ -129,6 +144,23 @@ def set_human_pose(index, hip_pose=None, hip_rot=None):
 
 	virtual_human.setState(reference, np.zeros([virtual_human.getDOF()]))
 
+	return reference
+
+def motion_matching_query(kd_tree, feature_mean, feature_std, 
+	current_root_pose, current_root_orientation, current_bone_pos, current_bone_velocity, 
+	desired_root_linear_velocity):
+	query_feature = np.zeros((1, 3 * 4 + 3 * 5))
+	frames = [19, 39, 59]
+	for i in range(3):
+		query_feature[:, i*4:i*4+2] = np.array([frames[i] * desired_root_linear_velocity[0], frames[i] * desired_root_linear_velocity[2]])
+		query_feature[:, i*4+2:i*4+4] = current_root_orientation.T.dot(np.array([1, 0, 0]))[[0, 2]]
+	query_feature[0, 3 * 4:3 * 4+3] = current_bone_velocity[0, :].flatten().copy()
+	query_feature[0, 3 * 4 + 3:3 * 4 + 6] = current_root_orientation.T.dot(current_bone_pos[17, :].flatten().copy() - current_root_pose)
+	query_feature[0, 3 * 4 + 6:3 * 4 + 9] = current_root_orientation.T.dot(current_bone_pos[21, :].flatten().copy() - current_root_pose)
+	query_feature[0, 3 * 4 + 9:3 * 4 + 12] = current_bone_velocity[17, :].copy()
+	query_feature[0, 3 * 4 + 12:3 * 4 + 15] = current_bone_velocity[21, :].copy()
+	dist, ind = kd_tree.query((query_feature-feature_mean) / feature_std, k=9)
+	return dist, ind
 
 balls = []
 for i in range(23):
@@ -136,66 +168,88 @@ for i in range(23):
 balls.append(world.addSphere(0.1, 0.8))
 
 
-matrix = motion.to_matrix(local=False)[:, :, :, :]
-local_matrix = motion.to_matrix(local=True)[:, :, : ,:]
-pos = matrix[:, :, :3, 3]
+# matrix = motion.to_matrix(local=False)[:, :, :, :]
+# local_matrix = motion.to_matrix(local=True)[:, :, : ,:]
+# pos = matrix[:, :, :3, 3]
 
-num_frames = matrix.shape[0]
+# num_frames = matrix.shape[0]
 
-root_pos = np.zeros((num_frames, 3))
-root_rot = np.zeros((num_frames, 3, 3))
-root_pos[:, 0] = pos[:, 0, 0]
-root_pos[:, 2] = pos[:, 0, 2]
+# root_pos = np.zeros((num_frames, 3))
+# root_rot = np.zeros((num_frames, 3, 3))
+# root_pos[:, 0] = pos[:, 0, 0]
+# root_pos[:, 2] = pos[:, 0, 2]
 
-root_rot[:, :, 0] = np.cross((pos[:, 15, :] - pos[:, 19, :]), (pos[:, 0, :] - pos[:, 1, :]))
-root_rot[:, 1, 0] = 0
-norms = np.apply_along_axis(np.linalg.norm, 1, root_rot[:, :, 0])
-root_rot[:, :, 0] /= norms[:, np.newaxis]
-root_rot[:, :, 1] = np.array([0, 1, 0])
-root_rot[:, :, 2] = np.cross(root_rot[:, :, 0], root_rot[:, :, 1])
+# root_rot[:, :, 0] = np.cross((pos[:, 15, :] - pos[:, 19, :]), (pos[:, 0, :] - pos[:, 1, :]))
+# root_rot[:, 1, 0] = 0
+# norms = np.apply_along_axis(np.linalg.norm, 1, root_rot[:, :, 0])
+# root_rot[:, :, 0] /= norms[:, np.newaxis]
+# root_rot[:, :, 1] = np.array([0, 1, 0])
+# root_rot[:, :, 2] = np.cross(root_rot[:, :, 0], root_rot[:, :, 1])
 
-store_data = np.zeros((num_frames, 23, 3))
-bone_velocity = np.zeros((num_frames, 23, 3))
-
-
-#bone position w.r.t the root
-for i in range(23):
-	A = np.transpose(root_rot[:, : :], axes=[0, 2, 1])
-	x = pos[:, i, :] - root_pos
-	# store_data[:, i, :] = np.einsum('ijk, ij->k', A, x)
-	store_data[:, i, :] = np.matmul(A, x[:, :, None]).squeeze()
-#bone velocity
-for i in range(num_frames-1):
-	for j in range(23):
-		bone_velocity[i, j, :] = store_data[i+1, j, :] - store_data[i, j, :]
+# store_data = np.zeros((num_frames, 23, 3))
+# bone_velocity = np.zeros((num_frames, 23, 3))
 
 
-#root velocity
-root_linear_vel = np.zeros((num_frames, 3))
-root_angular_vel = np.zeros((num_frames, 3, 3))
-root_angular_vel[-1, :, :] = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-for i in range(num_frames-1):
-	root_linear_vel[i] = root_rot[i, :, :].T.dot(root_pos[i+1, :] - root_pos[i, :])
-	root_angular_vel[i, :, :] = root_rot[i+1, :, :].dot(root_rot[i, :, :].T)
+# #bone position w.r.t the root
+# for i in range(23):
+# 	A = np.transpose(root_rot[:, : :], axes=[0, 2, 1])
+# 	x = pos[:, i, :] - root_pos
+# 	# store_data[:, i, :] = np.einsum('ijk, ij->k', A, x)
+# 	store_data[:, i, :] = np.matmul(A, x[:, :, None]).squeeze()
+# #bone velocity
+# for i in range(num_frames-1):
+# 	for j in range(23):
+# 		bone_velocity[i, j, :] = store_data[i+1, j, :] - store_data[i, j, :]
+
+
+# #root velocity
+# root_linear_vel = np.zeros((num_frames, 3))
+# root_angular_vel = np.zeros((num_frames, 3, 3))
+# root_angular_vel[-1, :, :] = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+# for i in range(num_frames-1):
+# 	root_linear_vel[i] = root_rot[i, :, :].T.dot(root_pos[i+1, :] - root_pos[i, :])
+# 	root_angular_vel[i, :, :] = root_rot[i+1, :, :].dot(root_rot[i, :, :].T)
+
+phase1 = np.loadtxt("NSM_phase/RunRandom.bvh/Phases_Standard.txt")[:-60, :]
+phase2 = np.loadtxt("NSM_phase/WalkSideBack2.bvh/Phases_Standard.txt")[:-60, :]
+phase3 = np.loadtxt("NSM_phase/WalkRandom.bvh/Phases_Standard.txt")[:-60, :]
+
+root_pos1, root_rot1, root_linear_vel1, root_angular_vel1, store_data1, bone_velocity1, local_matrix1, features1 = process_mocap("../../../../../Downloads/MotionCapture/Mocap/Loco/RunRandom.bvh", phase1)
+root_pos2, root_rot2, root_linear_vel2, root_angular_vel2, store_data2, bone_velocity2, local_matrix2, features2 = process_mocap("../../../../../Downloads/MotionCapture/Mocap/Loco/WalkSideBack2.bvh", phase2)
+root_pos3, root_rot3, root_linear_vel3, root_angular_vel3, store_data3, bone_velocity3, local_matrix3, features3 = process_mocap("../../../../../Downloads/MotionCapture/Mocap/Loco/WalkRandom.bvh", phase3)
+
+
+root_pos = np.concatenate((root_pos1, root_pos2, root_pos3))
+root_rot = np.concatenate((root_rot1, root_rot2, root_rot3))
+root_linear_vel = np.concatenate((root_linear_vel1, root_linear_vel2, root_linear_vel3))
+root_angular_vel = np.concatenate((root_angular_vel1, root_angular_vel2, root_angular_vel3))
+store_data = np.concatenate((store_data1, store_data2, store_data3))
+bone_velocity = np.concatenate((bone_velocity1, bone_velocity2, bone_velocity3))
+local_matrix = np.concatenate((local_matrix1, local_matrix2, local_matrix3))
+features = np.concatenate((features1, features2, features3))
+phase = np.concatenate((phase1, phase2, phase3))
+num_frames = root_pos.shape[0]
 
 #build feature space for kd tree
-features = np.zeros((num_frames - 60, 3 * 4 + 3 * 5))
-for i in range(num_frames - 60):
-	frames = [19, 39, 59]
-	root_pos_feature = np.zeros(3)
-	for j in range(3):
-		features[i, j*4:j*4+2] = root_rot[i, :, :].T.dot(root_pos[frames[j] + i] - root_pos[i])[[0, 2]]
-		features[i, j*4+2:j*4+4] = root_rot[i+frames[j], :, :].dot(root_rot[i, :, :].T).dot(np.array([1, 0, 0]))[[0, 2]]
-		#features[i, j*9+6:j*9+9] = root_angular_vel[i+j, :, 1].copy()
-	features[i, 3 * 4: 3 * 4 + 3] = bone_velocity[i, 0, :].flatten().copy()
-	features[i, 3 * 4 + 3: 3 * 4 + 6] = store_data[i, 17, :].flatten().copy()
-	features[i, 3 * 4 + 6: 3 * 4 + 9] = store_data[i, 21, :].flatten().copy()
-	features[i, 3 * 4 + 9: 3 * 4 + 12] = bone_velocity[i, 17, :].flatten().copy()
-	features[i, 3 * 4 + 12: 3 * 4 + 15] = bone_velocity[i, 21, :].flatten().copy()
+# features = np.zeros((num_frames - 60, 3 * 4 + 3 * 5))
+# for i in range(num_frames - 60):
+# 	frames = [19, 39, 59]
+# 	root_pos_feature = np.zeros(3)
+# 	for j in range(3):
+# 		features[i, j*4:j*4+2] = root_rot[i, :, :].T.dot(root_pos[frames[j] + i] - root_pos[i])[[0, 2]]
+# 		features[i, j*4+2:j*4+4] = root_rot[i+frames[j], :, :].dot(root_rot[i, :, :].T).dot(np.array([1, 0, 0]))[[0, 2]]
+# 		#features[i, j*9+6:j*9+9] = root_angular_vel[i+j, :, 1].copy()
+# 	features[i, 3 * 4: 3 * 4 + 3] = bone_velocity[i, 0, :].flatten().copy()
+# 	features[i, 3 * 4 + 3: 3 * 4 + 6] = store_data[i, 17, :].flatten().copy()
+# 	features[i, 3 * 4 + 6: 3 * 4 + 9] = store_data[i, 21, :].flatten().copy()
+# 	features[i, 3 * 4 + 9: 3 * 4 + 12] = bone_velocity[i, 17, :].flatten().copy()
+# 	features[i, 3 * 4 + 12: 3 * 4 + 15] = bone_velocity[i, 21, :].flatten().copy()
 
 feature_mean = np.mean(features, axis=0)
 feature_std = np.std(features, axis=0)
-feature_std[0:12] /= 1
+feature_std[2:4] /= 2
+feature_std[4:6] /= 2
+# feature_std[4:6] /= 2
 for i in range(feature_std.shape[0]):
 	if abs(feature_std[i]) < 0.00001:
 		feature_std[i] = 1
@@ -213,24 +267,19 @@ world_vel = np.zeros(3)
 
 #kd tree query
 current_root_pose = np.zeros(3)
-desired_root_linear_velocity = np.array([10, 0, 0])
+desired_root_linear_velocity = np.array([20, 0, 0])
 current_bone_pos = store_data[0, :, :].copy()
 current_bone_velocity = bone_velocity[0, :, :].copy()
 current_root_orientation = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
 desired_root_delta_orientation = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
+
+save_frames = 60
+save_reference = np.zeros((save_frames, 75))
+frame_counter = 0
 while True:
-	#build feature for query
-	query_feature = np.zeros((1, 3 * 4 + 3 * 5))
-	frames = [19, 39, 59]
-	for i in range(3):
-		query_feature[:, i*4:i*4+2] = np.array([frames[i] * desired_root_linear_velocity[0], frames[i] * desired_root_linear_velocity[2]])
-		query_feature[:, i*4+2:i*4+4] = current_root_orientation.T.dot(np.array([1, 0, 0]))[[0, 2]]
-	query_feature[0, 3 * 4:3 * 4+3] = current_bone_velocity[0, :].flatten().copy()
-	query_feature[0, 3 * 4 + 3:3 * 4 + 6] = current_root_orientation.T.dot(current_bone_pos[17, :].flatten().copy() - current_root_pose)
-	query_feature[0, 3 * 4 + 6:3 * 4 + 9] = current_root_orientation.T.dot(current_bone_pos[21, :].flatten().copy() - current_root_pose)
-	query_feature[0, 3 * 4 + 9:3 * 4 + 12] = current_bone_velocity[17, :].copy()
-	query_feature[0, 3 * 4 + 12:3 * 4 + 15] = current_bone_velocity[21, :].copy()
-	dist, ind = tree.query((query_feature-feature_mean) / feature_std, k=9)
+	dist, ind = motion_matching_query(tree, feature_mean, feature_std, 
+		current_root_pose, current_root_orientation, current_bone_pos, current_bone_velocity, 
+		desired_root_linear_velocity)
 	for t in range(20):
 		# current_root_pose += desired_root_linear_velocity / 10
 		# current_root_orientation = current_root_orientation.dot(desired_root_delta_orientation)
@@ -238,7 +287,7 @@ while True:
 		current_root_pose += current_root_orientation.dot(root_linear_vel[ind[0,0]+t-1])
 		current_root_orientation = root_angular_vel[ind[0,0]+t-1, :, :].dot(current_root_orientation)
 		
-		print(dist, ind)
+		# print(dist, ind)
 		for i in range(23):
 			current_bone_pos[i, :] = ((store_data[ind[0,0]+1+t, i, :] + current_root_pose))
 			balls[i].setPosition(current_bone_pos[i, 0]/200, current_bone_pos[i, 2]/200, current_bone_pos[i, 1]/200)
@@ -246,7 +295,13 @@ while True:
 		current_bone_velocity[17, :] = bone_velocity[ind[0, 0]+1+t, 17, :].copy()
 		current_bone_velocity[21, :] = bone_velocity[ind[0, 0]+1+t, 21, :].copy()
 
-		set_human_pose(ind[0, 0]+1+t, current_bone_pos[0, :], current_root_orientation)
+		reference = set_human_pose(ind[0, 0]+1+t, current_bone_pos[0, :], current_root_orientation)
+
+		# save_reference[frame_counter, :] = reference.copy()
+		# frame_counter += 1
+		# if (frame_counter == save_frames):
+		# 	with open("reference.npy", 'wb') as f:
+		# 		np.save(f, save_reference)
 
 		world.integrate()
 		import time; time.sleep(0.016)
